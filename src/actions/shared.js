53859f4f97e8702053e808b456d9074dae0b9482
Vijayanand Settin <stvijayanand@hotmail.com>
2019-04-15 14:44:06 -0700

    feat: complete actions/shard

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,9 +1,12 @@
-import {getInitialData} from "../utils/api"
+import { getInitialData } from "../utils/api";
+import { receiveUsers } from "../actions/users";
+import { receiveTweets } from "../actions/tweets";
 
 export function handleInitialData() {
-    return dispatch => {
-        return getInitialData()
-                .then(({users, tweets}) => {
-        })
-    }
+  return dispatch => {
+    return getInitialData().then(({ users, tweets }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveTweets(tweets));
+    });
+  };
 }
