f2a3359ebc95961cba4c403f169b9b8aa2375fae
Vijayanand Settin <stvijayanand@hotmail.com>
2019-04-16 10:43:28 -0700

    feat: connect and handleInitialData

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -5,7 +5,7 @@ import { setAuthedUser } from "../actions/authedUser";
 
 const AUTHED_ID = "tylermcginnis";
 
-export function handleInitialData() {
+export default function handleInitialData() {
   return dispatch => {
     return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
