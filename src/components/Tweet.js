0cf4dd20f14f3fec64462b61cd8854a71be16440
VIjayanand Settin <stvijayanand@hotmail.com>
2019-04-18 15:55:27 -0700

    feat: ReplyingTo withRouter

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,7 +5,7 @@ import {TiArrowBackOutline} from 'react-icons/ti'
 import {TiHeartOutline} from 'react-icons/ti'
 import {TiHeartFullOutline} from 'react-icons/ti'
 import {handleToggleTweet} from "../actions/tweets"
-import {Link} from "react-router-dom"
+import {Link, withRouter} from "react-router-dom"
 
 class Tweet extends Component {
   handleLike = (e) => {
@@ -20,8 +20,9 @@ class Tweet extends Component {
   }
 
   toParent = (e, id) => {
-    e.preventDefault()
-    // todo: Redirect to parent Tweet.
+    e.preventDefault();
+
+    this.props.history.push(`/tweet/${id}`);
   }
 
   render() {
@@ -81,4 +82,4 @@ const mapStateToProps = ({ authedUser, tweets, users }, { id }) => {
   };
 };
 
-export default connect(mapStateToProps)(Tweet);
+export default withRouter(connect(mapStateToProps)(Tweet))
