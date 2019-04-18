6bf0644e24d605521648699906149f23e05e59ed
Vijayanand Settin <stvijayanand@hotmail.com>
2019-04-17 17:00:21 -0700

    feat: Tweet component - 2

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import authedUser from "../reducers/authedUser";
-import tweets from "../reducers/tweets";
 import { formatTweet } from "../utils/helpers";
 
 class Tweet extends Component {
   render() {
+    const { tweet } = this.props;
+
+    if (tweet === null) {
+      return <p>This tweet doesn't exist</p>;
+    }
+
     console.log(this.props);
     return <div className="tweet" />;
   }
@@ -13,10 +17,13 @@ class Tweet extends Component {
 
 const mapStateToProps = ({ authedUser, tweets, users }, { id }) => {
   const tweet = tweets[id];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser)
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null
   };
 };
 
